49e541ed53dfc1412c6ba1d0ebc642f41fc01365
Jason Kirsons <jkirsons@gmail.com>
2018-09-10 19:44:38 +0200

    change CS

--- src/main.cpp
+++ src/main.cpp
@@ -243,7 +243,8 @@ void setup() {
   stext2.setTextColor(TFT_WHITE); // White text, no background
 
   Serial.begin(115200);
-  if(!SD.begin()){
+  SPIClass &spi=SPI;
+  if(!SD.begin(15, spi, 4000000, "/sd")){
       Serial.println("Card Mount Failed");
       return;
   }
@@ -269,19 +270,12 @@ void setup() {
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
   listDir(SD, "/", 0);
-  createDir(SD, "/mydir");
-  listDir(SD, "/", 0);
-  removeDir(SD, "/mydir");
-  listDir(SD, "/", 2);
   writeFile(SD, "/hello.txt", "Hello ");
   appendFile(SD, "/hello.txt", "World!\n");
   readFile(SD, "/hello.txt");
-  deleteFile(SD, "/foo.txt");
-  renameFile(SD, "/hello.txt", "/foo.txt");
-  readFile(SD, "/foo.txt");
-  testFileIO(SD, "/test.txt");
   Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
   Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  SD.end();
 }
 
 //==========================================================================================
